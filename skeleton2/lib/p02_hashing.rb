a220208d837e7c4fa1efb054bda5a16e7e21e737
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-26 14:52:18 -0500

    Finish hashing

--- skeleton2/lib/p02_hashing.rb
+++ skeleton2/lib/p02_hashing.rb
@@ -3,19 +3,42 @@ class Fixnum
 end
 
 class Array
-  def hash
+  def hash()
+  #   self.each_with_index do |el, index|
+  #     if el.is_a?(Array)
+  #       counter += 1
+  #       el.hash(result, counter)
+  #       counter.times  { result /=  2}
+  #     elsif el.is_a?(String)
+  #       result += el.hash + (index * 11)
+  #     else
+  #       result += el + (index * 11)
+  #     end
+  #   end
+  #   result
+  # end
+      self.each_with_index.reduce(100) do |acc, (el, index)|
+        acc += el.hash * (index + 1).hash
+      end
+
   end
 end
 
 class String
   def hash
+    result = 0
+    self.each_char.with_index do |char, index|
+      result += char.ord * (index + 1 * 11)
+    end
+    result
   end
+
 end
 
 class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.hash
   end
 end
